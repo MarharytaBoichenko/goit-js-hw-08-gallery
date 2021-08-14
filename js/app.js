02bcf032044d214673dcc585bdd1adb58d28fa9e
MarharytaBoichenko <boichenkomv@gmail.com>
2021-08-14 15:16:34 +0300

    save all

--- js/app.js
+++ js/app.js
@@ -64,6 +64,7 @@ const galleryItems = [
   },
 ];
 
+
 const galleryEl = document.querySelector(".js-gallery")
 console.log(galleryEl);
 const galleryMarkup = createGalleryMarkup(galleryItems);
@@ -73,22 +74,22 @@ console.log(galleryMarkup);
 
 function createGalleryMarkup(gallery) {
   return gallery.map(({ preview, original, description }) => 
-   
     `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
   >
     <img
-      class="gallery__image"
-      src="${preview}"
+      class="gallery__image lazyload"
+      data-src="${preview}"
       data-source="${original}"
+      loading = "lazy"
       alt="${description}"
+
     />
   </a>
 </li>`
   ).join("");
-  
 };
 
 const modalEl = document.querySelector(".lightbox")
@@ -105,20 +106,18 @@ overlayEl.addEventListener('click', onBtnModalClose)
 
 function onImgLinkClick(e) {
   e.preventDefault();
-  modalEl.classList.add("is-open");
   if (e.target.nodeName !== "IMG") {
     return;
   }
-
-  window.addEventListener('keydown', onEscapeClose);
+  modalEl.classList.add("is-open");
   modalImg.src = e.target.dataset.source;
+  window.addEventListener('keydown', onEscapeClose);
 };
 
 function onBtnModalClose(e) {
 window.removeEventListener('keydown', onEscapeClose);
   modalEl.classList.remove("is-open");
   modalImg.src === "";
-  //Очистка значения атрибута src элемента img.lightbox__image
 }
 
 function onEscapeClose(e) {
@@ -135,64 +134,81 @@ function onBackdropClose(e) {
   }
 }
 
-//пролистывание 
-
-
-
-
+//пролистывание
 window.addEventListener('keydown', onRightPress);
-window.addEventListener("keydown", onLeftPress)
-
-
+window.addEventListener("keydown", onLeftPress);
 
 function onRightPress({ code }) {
 //картинка,  что сейчас открыта  в модалке
   const currentImgRef = galleryEl
     .querySelector(`[data-source="${modalImg.src}"]`);
   
-  console.log(currentImgRef);
-
+  // console.log(currentImgRef);
   //лишка-предок от которой будем  искать след  эл-т 
   const parentNode = currentImgRef.closest('.gallery__item');
-  console.log(parentNode);
-  
-  //лед картинка - это выбрать след лишку,  если она есть(если не последняя
+  // console.log(parentNode);
+  //cлед картинка - это выбрать след лишку,  если она есть(если не последняя
   // и в этом эл-те ли  найти  эл-т  gallery-img)
   const nextImgRef = parentNode.nextElementSibling?.querySelector('.gallery__image');
-  console.log(nextImgRef);
-
+  // console.log(nextImgRef);
   // ссылка  на  изображение  в найденной след  картинке -  это  то, что  находится в атрибуте data-source
   console.log(nextImgRef.dataset.source);
 
  //если нажата клавиif Вправо и если след картинка  существует,  то подменяем  ссылку  в модалке на ссылку след  картинки
   if (code === 'ArrowRight' && nextImgRef) {
-      modalImg.src = nextImgRef.dataset.source
-      
-  } //а  если  нет  дальше эл-та -??? что  сделать чтоб консоль не выдавала ошибку??
-
+      modalImg.src = nextImgRef.dataset.source  
+  } return;
+  //а  если  нет  дальше эл-та -??? что  сделать чтоб консоль не выдавала ошибку??
 };
 
 function onLeftPress({ code }) {
   const currentImgRef = galleryEl.querySelector(`[data-source="${modalImg.src}"]`);
-  console.log(currentImgRef);
-
   const parentNode = currentImgRef.closest(".gallery__item");
-  console.log(parentNode);
-
   const previousImgRef = parentNode.previousElementSibling?.querySelector(".gallery__image");
-  console.log(previousImgRef);
-
-  ////может так? 
+  ////может так лучше? 
   if (code !== "ArrowLeft") {
     return;
   }
-    if (previousImgRef) {
-      modalImg.src = previousImgRef.dataset.source; 
-    } 
-   
+    if (!previousImgRef) { 
+      return;
+    }  modalImg.src = previousImgRef.dataset.source; 
+    
 }
-///почему  при нажатии любой    стрелки отрабатывают консоли двух функций??
-///и при нажатии  любой клавиши ///
 
   
 
+///ленивая загрузка 
+// в разметке каждой  картинке поставить  атрибут   loading = "lazy"
+
+// можем прослушать событие загрузки картинки и   сделать например анимацию, когда картинка загрузилась
+
+// картинка грузится ж один раз ,  поэтому слуш события  удаляется чтобы он дальше не висел , для этого  once: true
+// сафари  не  поддерживает ленивки,   потому надо  не атрибутом, а пдключать библиотеку  lazysizes  
+
+//библиотеку подключаем по условию 
+const lazyImages = document.querySelectorAll('img[loading = "lazy"]');
+
+if ('loading' in HTMLImageElement.prototype) {
+  console.log(`поддерживает`);
+  lazyImages.forEach(img => {
+    img.src = img.dataset.src;
+  })
+} else {
+  console.log(`НЕТ`)
+  const script = document.createElement("script");
+  script.scr = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+  document.body.appendChild(script);
+} 
+
+lazyImages.forEach(img => {
+  img.addEventListener('load', onImgLoaded, {once: true})
+});
+
+function onImgLoaded(e) {
+  console.log(e);
+  console.log("lazyImages");
+  e.target.classList.add("is-loaded")
+}
